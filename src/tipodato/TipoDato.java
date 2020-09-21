663a16af93b26f8445e87473dbf08f03f0121ecc
xedinsonx <xedinsonx2@gmail.com>
2020-09-21 18:59:55 -0500

    Operadores

---
+++ src/tipodato/TipoDato.java
@@ -0,0 +1,30 @@
+
+package tipodato;
+
+
+public class TipoDato {
+
+    
+    public static void main(String[] args) {
+        
+        int num1  = 20;
+        int num2 = 10;
+        int suma; 
+        int resta;
+        int multiplicacion;
+        double division;
+        
+        suma = num1 + num2;
+        resta = num1 - num2;
+        multiplicacion = num1 * num2;
+        division = num1 / num2;
+        
+        System.out.println("La Adiccion es:" +suma);
+        System.out.println("La Resta es:" +resta);
+        System.out.println("El Producto es:" +multiplicacion);
+        System.out.println("La Division es:" +division);
+        
+       
+    }
+    
+}
